b756f2f24263c27ea51cf72de3e9df57f9592467
Ekkana <alexeykerpel15@gmail.com>
2024-12-07 18:30:53 -0500

    Improved speed by 20000 times

--- day7/index.js
+++ day7/index.js
@@ -2,14 +2,17 @@ const path = require("path");
 const fs = require("fs");
 
 const input = fs
-  .readFileSync(path.join(__dirname, "input2.txt"), "utf8")
+  .readFileSync(path.join(__dirname, "input1.txt"), "utf8")
   .toString()
   .trim();
 
 const splitLines = input.split("\n");
 const arr = splitLines.map((line) => [
   parseInt(line.split(": ")[0]),
-  line.split(": ")[1].split(" "),
+  line
+    .split(": ")[1]
+    .split(" ")
+    .map((el) => parseInt(el)),
 ]);
 
 const part1 = () => {
@@ -76,6 +79,9 @@ const part2 = () => {
         }
 
         current = Math.floor(current / operations.length);
+        if (parseInt(resultStr) > result) {
+          break;
+        }
       }
 
       if (resultStr == result) {
@@ -86,8 +92,103 @@ const part2 = () => {
     }
     resultStr = "";
   }
+  console.log(output.reduce((acc, curr) => acc + curr, 0));
 };
 
 console.time("part2");
 part2();
 console.timeEnd("part2");
+
+const part3 = () => {
+  const output = [];
+
+  const findAnswer = (arr, curIndex, prevValue, result, operationFunc) => {
+    const curVal = operationFunc(prevValue, arr[curIndex]);
+    if (curVal > result) {
+      return false;
+    }
+
+    if (arr.length === curIndex) {
+      return prevValue === result;
+    }
+
+    return (
+      findAnswer(arr, curIndex + 1, curVal, result, sum) ||
+      findAnswer(arr, curIndex + 1, curVal, result, mul) ||
+      findAnswer(arr, curIndex + 1, curVal, result, concat)
+    );
+  };
+
+  const sum = (a, b) => a + b;
+  const mul = (a, b) => a * b;
+  const concat = (a, b) => parseInt(`${a}${b}`);
+
+  for (let i = 0; i < arr.length; i++) {
+    const result = arr[i][0];
+    const items = arr[i][1];
+
+    if (findAnswer(items, 0, 0, result, sum)) {
+      output.push(result);
+    }
+  }
+
+  console.log(output.reduce((acc, curr) => acc + curr, 0));
+};
+console.time("part3");
+part3();
+console.timeEnd("part3");
+
+const part4 = () => {
+  const output = [];
+
+  const findAnswer = (arr, curIndex, prevValue, operationFunc) => {
+    if (0 === curIndex) {
+      return prevValue === arr[0];
+    }
+    const curVal = operationFunc(prevValue, arr[curIndex]);
+
+    if (curVal == 0) {
+      //console.log("Not valid", curVal, prevValue, arr[curIndex]);
+      return false;
+    }
+
+    return (
+      findAnswer(arr, curIndex - 1, curVal, sub) ||
+      findAnswer(arr, curIndex - 1, curVal, div) ||
+      findAnswer(arr, curIndex - 1, curVal, split)
+    );
+  };
+
+  const sub = (a, b) => {
+    const val = a - b;
+    return val > 0 ? val : 0;
+  };
+  const div = (a, b) => {
+    const val = a / b;
+    return Math.floor(val) == val ? val : 0;
+  };
+  const split = (a, b) => {
+    const aStr = a.toString();
+    const bStr = b.toString();
+    const isValid = aStr.substring(aStr.length - bStr.length) === bStr;
+
+    return isValid ? parseInt(aStr.substring(0, aStr.length - bStr.length)) : 0;
+  };
+
+  for (let i = 0; i < arr.length; i++) {
+    const result = arr[i][0];
+    const items = arr[i][1];
+    if (
+      findAnswer(items, items.length - 1, result, sub) ||
+      findAnswer(items, items.length - 1, result, div) ||
+      findAnswer(items, items.length - 1, result, split)
+    ) {
+      output.push(result);
+    }
+  }
+
+  console.log(output.reduce((acc, curr) => acc + curr, 0));
+};
+console.time("part4");
+part4();
+console.timeEnd("part4");

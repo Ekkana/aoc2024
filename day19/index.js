a46989ff599d40c4a00ba88b54641f1f219d826d
Ekkana <alexeykerpel15@gmail.com>
2024-12-19 14:32:20 -0500

    day19

---
+++ day19/index.js
@@ -0,0 +1,159 @@
+import { parseInput } from "./parseInput.js";
+
+const [towels, target] = parseInput("input1.txt");
+
+console.time("part2_sol3");
+part2_sol3();
+console.timeEnd("part2_sol3");
+
+function part2_sol3() {
+  const canCombine = (
+    target,
+    towels,
+    currentStr = "",
+    count = 0,
+    visited = {},
+  ) => {
+    if (typeof visited[currentStr] !== "undefined") {
+      return visited[currentStr];
+    }
+
+    if (currentStr === target) {
+      return count + 1;
+    }
+
+    if (!target.startsWith(currentStr)) {
+      return count;
+    }
+
+    let total = 0;
+
+    for (let i = 0; i < towels.length; i++) {
+      const newStr = currentStr + towels[i];
+      total += canCombine(target, towels, newStr, count, visited);
+    }
+
+    visited[currentStr] = total;
+
+    return total;
+  };
+
+  let count = 0;
+
+  for (let i = 0; i < target.length; i++) {
+    let data = canCombine(target[i], towels);
+    count += data;
+  }
+
+  console.log(count);
+}
+
+function part2_sol2() {
+  const canCombine = (target, towels, currentStr, count, visited) => {
+    if (currentStr === target) {
+      return count + 1;
+    }
+
+    if (!target.startsWith(currentStr)) {
+      visited[currentStr] = false;
+      return count;
+    }
+
+    visited[currentStr] = true;
+
+    let total = 0;
+
+    for (let i = 0; i < towels.length; i++) {
+      const newStr = currentStr + towels[i];
+      total += canCombine(target, towels, newStr, count, visited);
+    }
+
+    console.log(visited);
+
+    return total;
+  };
+
+  let count = 0;
+
+  for (let i = 0; i < target.length; i++) {
+    const towelsObject = towels.reduce((acc, curr) => {
+      acc[curr] = true;
+      return acc;
+    }, {});
+    let data = canCombine(target[i], towels, "", 0, towelsObject);
+    count += data;
+    console.log(data);
+  }
+
+  console.log(count);
+}
+
+//console.time("part2");
+//part2();
+//console.timeEnd("part2");
+
+function part2() {
+  const canCombine = (target, towels, currentStr, count, visited) => {
+    if (currentStr === target) {
+      return count + 1;
+    }
+
+    let total = 0;
+
+    for (let i = 0; i < towels.length; i++) {
+      const newStr = currentStr + towels[i];
+      if (target.startsWith(newStr)) {
+        total += canCombine(target, towels, newStr, count, visited);
+      }
+    }
+
+    return total;
+  };
+
+  let count = 0;
+
+  for (let i = 0; i < target.length; i++) {
+    const visited = [];
+    let data = canCombine(target[i], towels, "", 0, visited);
+    count += data;
+    console.log(data);
+  }
+
+  console.log(count);
+}
+
+console.time("part1");
+part1();
+console.timeEnd("part1");
+
+function part1() {
+  const canCombine = (target, towels, currentStr) => {
+    if (currentStr === target) {
+      return true;
+    }
+    if (!target.startsWith(currentStr)) {
+      return false;
+    }
+    if (currentStr.length > target.length) {
+      return false;
+    }
+
+    for (let i = 0; i < towels.length; i++) {
+      if (canCombine(target, towels, currentStr + towels[i])) {
+        return true;
+      }
+    }
+
+    return false;
+  };
+
+  let count = 0;
+
+  for (let i = 0; i < target.length; i++) {
+    if (canCombine(target[i], towels, "")) {
+      count++;
+    }
+  }
+
+  console.log(count);
+}
---
+++ day19/parseInput.js
@@ -0,0 +1,20 @@
+import path from "path";
+import fs from "fs";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export const parseInput = (fileName) => {
+  const inputRules = fs
+    .readFileSync(path.join(__dirname, fileName), "utf8")
+    .toString()
+    .trim();
+
+  const [towelsStr, targetStr] = inputRules.split("\n\n");
+  const towels = towelsStr.split(", ");
+  const target = targetStr.split("\n");
+
+  return [towels, target];
+};
